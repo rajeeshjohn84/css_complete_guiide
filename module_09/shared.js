8445c4b83a924ebb2353c4231556d58c51e9fc5d
John Constantine <28708924+nthequilibrium@users.noreply.github.com>
2021-12-20 17:14:51 +0530

    adding event listeners to buttons

--- module_09/shared.js
+++ module_09/shared.js
@@ -1,5 +1,22 @@
 const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
 
-// console.dir(backdrop);
+const modalNoBtn = document.querySelector(".modal__action--negative");
 
-backdrop.style.display = "block";
+const selectPlanBtns = document.querySelectorAll(".plan button");
+
+selectPlanBtns.forEach(btn => {
+  btn.addEventListener("click", e => {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+});
+
+backdrop.addEventListener("click", closeModal);
+
+modalNoBtn.addEventListener("click", closeModal);
+
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+}
