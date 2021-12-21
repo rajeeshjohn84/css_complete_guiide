ca5bb306f88189bdce6ae7bd5b00d84d49828b05
John Constantine <28708924+nthequilibrium@users.noreply.github.com>
2021-12-21 11:38:12 +0530

    making site responsive initial setup

---
+++ module_10/shared.js
@@ -0,0 +1,45 @@
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+
+const modalNoBtn = document.querySelector(".modal__action--negative");
+
+const selectPlanBtns = document.querySelectorAll(".plan button");
+
+const toggleBtn = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
+
+selectPlanBtns.forEach(btn => {
+  btn.addEventListener("click", e => {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = "open"; // overrides complete classList
+    modal.classList.add("open");
+    backdrop.classList.add("open");
+  });
+});
+
+backdrop.addEventListener("click", function () {
+  // mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
+  closeModal();
+});
+
+if (modalNoBtn) {
+  modalNoBtn.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+  // backdrop.style.display = "none";
+  // modal.style.display = "none";
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
+}
+
+toggleBtn.addEventListener("click", function () {
+  // mobileNav.style.display = "block";
+  // backdrop.style.display = "block";
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
+});
